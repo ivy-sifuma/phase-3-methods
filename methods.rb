0758b840551753c8cc51ca4f7262261a58004b6d
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-17 21:40:05 +0300

    Methods

--- methods.rb
+++ methods.rb
@@ -1,1 +1,28 @@
 # Your code here!
+  #greetProgrammer
+  def greetProgrammer
+  puts  "Hello, programmer!"
+
+  #greet
+  def greet(name = "Ivy") 
+  puts "Hello, #{name}!"
+  end
+
+  #greet_with_default
+  def greetWithDefault(name = "programmer") 
+  puts "Hello, #{name}!"
+  end
+
+   #add
+   def add(num1, num2) 
+  return num1 + num2
+   end
+
+   #halve
+   def halve(num1) 
+     if num1.class != Integer
+        return nil
+     end
+     num1=6
+     num1/2
+    end
