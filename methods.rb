75c3b435a630992c3ccd4fe14f04f9306786559c
Ivy Sifuma <ivy.sifuma@student.moringaschool.com>
2022-10-25 17:27:48 +0300

    Completed

--- methods.rb
+++ methods.rb
@@ -2,28 +2,38 @@
 
   #greetProgrammer
   def greetProgrammer
-  puts  "Hello, programmer!"
+  puts  ("Hello, programmer!")
+  end
+  greet_programmer
 
   #greet
-  def greet(name = "Ivy") 
+  def greet(name = "Naureen") 
   puts "Hello, #{name}!"
   end
+
+  greet
+  greet "Jacob"
 
   #greet_with_default
   def greetWithDefault(name = "programmer") 
   puts "Hello, #{name}!"
   end
 
+  greet_with_default
+
+  greet_with_default "Naureen"
+
    #add
    def add(num1, num2) 
   return num1 + num2
    end
+   add(2,5)
 
    #halve
    def halve(num1) 
      if num1.class != Integer
         return nil
      end
-     num1=6
      num1/2
     end
+    halve(6)
